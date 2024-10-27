0df136a1eabf4342cb4298baf0962990c40052a6
Dori Lahav Waisberg <dori.lahav@gmail.com>
2024-10-27 23:06:19 +0200

    Create flow test - all worked!!

--- packages/rabbit/src/index.ts
+++ packages/rabbit/src/index.ts
@@ -1,22 +1,30 @@
 import { RabbitMQClient } from './client';
 import { createDeclaration } from './declarations';
 
-const queue1Declaration = createDeclaration(
+const sourceExchange = 'one';
+const middleExchange = 'two';
+const targetQueue = 'three';
+
+const consumer = () => {
+  console.log('shit');
+}
+
+const consumerDeclaration = createDeclaration(
   setup => {
-    setup.declareQueue({name: 'queue-1'})
-      .bindQueueToExchange({sourceExchange: 'dd', targetQueue: 'queue-1'})
-      .consume({
-        queueName: 'queue-1',
-        consumer() {
-          console.log('something happened')
-        }
-      })
+    setup
+      .declareExchange({name: sourceExchange})
+      .declareExchange({name: middleExchange})
+      .declareQueue({name: targetQueue})
+      .bindExchangeToExchange({sourceExchange, targetExchange: middleExchange})
+      .bindQueueToExchange({sourceExchange: middleExchange, targetQueue})
+      .consume({queueName: targetQueue, consumer});
   }
 );
 
 
-const client = new RabbitMQClient();
+const client = new RabbitMQClient({url: 'amqp://makmachat-realtime:i9kgRdBE84eq5JdK@compute-1.k8s.dorilahav.com:30672/makmachat-test'});
 
-client.declare(queue1Declaration);
+client.declare(consumerDeclaration);
 
-console.log(client);
+client.connect()
+  .then(() => console.log('Connected!'));
